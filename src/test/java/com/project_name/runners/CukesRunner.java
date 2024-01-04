cb48bcd9840e5c345a678976fbe6628ae5c53cd6
AdamB25 <barsialtunit@gmail.com>
2024-01-04 17:41:12 -0500

    project_template

--- src/test/java/com/project_name/runners/CukesRunner.java
+++ src/test/java/com/project_name/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/project_name/step_definitions",
         dryRun = true,
-        tags = " @wip",
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
