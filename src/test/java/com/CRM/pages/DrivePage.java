8d3ddf0baa823014c82b85c5cc7ace14cea5d37e
dkamin212 <dkamin212@gmail.com>
2024-03-22 11:11:16 -0400

    Finish DrivePage, U13 Feature Page, and StepDefs and update Base Page

--- src/test/java/com/CRM/pages/DrivePage.java
+++ src/test/java/com/CRM/pages/DrivePage.java
@@ -1,7 +1,11 @@
 package com.CRM.pages;
 
 import com.CRM.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class DrivePage {
 
@@ -9,6 +13,13 @@ public class DrivePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-
+    @FindBy(xpath = "//tr[@class='bx-layout-inner-inner-top-row']//div[@class='main-buttons']//div[@id='top_menu_id_docs_menu_my_disk'] | \n" +
+            "//div[@id='top_menu_id_docs_63494979'] | \n" +
+            "//div[@id='top_menu_id_docs_414529032'] | \n" +
+            "//div[@id='top_menu_id_docs_more_button'] | \n" +
+            "//div[@id='top_menu_id_docs_2673172489'] | \n" +
+            "//div[@id='top_menu_id_docs_3526888156'] | \n" +
+            "//div[@id='top_menu_id_docs_menu_my_disk_volume']\n")
+    public List<WebElement> DrivePageModules;
 
 }
--- src/test/java/com/CRM/step_definitions/US13_DrivePageModuleAccess_StepDefs.java
+++ src/test/java/com/CRM/step_definitions/US13_DrivePageModuleAccess_StepDefs.java
@@ -1,26 +1,52 @@
 package com.CRM.step_definitions;
 
+import com.CRM.pages.BasePage;
 import com.CRM.pages.DrivePage;
+import com.CRM.pages.LoginPage;
+import com.CRM.utilities.BrowserUtils;
+import com.CRM.utilities.ConfigurationReader;
+import com.CRM.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class US13_DrivePageModuleAccess_StepDefs {
+public class US13_DrivePageModuleAccess_StepDefs extends BasePage {
 
     DrivePage drivePage = new DrivePage();
+    LoginPage loginPage = new LoginPage();
 
-    @When("User should go to the Drive page")
-    public void i_go_to_the_drive_page() {
-
+    @When("{string} goes to the Drive page")
+    public void goesToTheDrivePage(String userType) {
+        if (userType.contains("HR")){
+            loginPage.login(ConfigurationReader.getProperty("hr_username"), ConfigurationReader.getProperty("hr_password"));
+        } else if (userType.contains("Help")){
+            loginPage.login(ConfigurationReader.getProperty("helpdesk_username"), ConfigurationReader.getProperty("helpdesk_password"));
+        } else if (userType.contains("Marketing")){
+            loginPage.login(ConfigurationReader.getProperty("marketing_username"), ConfigurationReader.getProperty("marketing_password"));
+        } //Changes depending on username in the feature page
+        DrivePageLink.click(); //Using the abstract class of BasePage that contains the main page information to first navigate to the drive page
     }
+
+
     @Then("User should see the Drive page")
     public void i_should_see_the_drive_page() {
-
+        BrowserUtils.verifyTitleContains("Drive"); //Checking the is correctly landing on the drive page
     }
 
-    @Then("User should see all the following modules")
+    @Then("User should see all the following modules:")
     public void userShouldSeeAllTheFollowingModules(List<String> expectedTopModules) {
+
+        List<String> actualTopModules = new ArrayList<>();
+        for (WebElement each : drivePage.DrivePageModules) {
+            actualTopModules.add(each.getText());
+        } //Loop through actual top modules
+
+        Assert.assertEquals(actualTopModules, expectedTopModules);
 
     }
 }
