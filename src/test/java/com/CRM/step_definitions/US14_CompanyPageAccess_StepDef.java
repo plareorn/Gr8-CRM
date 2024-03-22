090ee5c65e64ea02f6cc8a6e1b654108ceee0ac0
mohamedbrz <mohamed.bourazza1@gmail.com>
2024-03-22 07:25:20 -0400

    Company page access and verification of the modules [completed]

--- src/test/java/com/CRM/step_definitions/US14_CompanyPageAccess_StepDef.java
+++ src/test/java/com/CRM/step_definitions/US14_CompanyPageAccess_StepDef.java
@@ -1,4 +1,45 @@
 package com.CRM.step_definitions;
 
+import com.CRM.pages.CompanyPage;
+import com.CRM.pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class US14_CompanyPageAccess_StepDef {
+
+    CompanyPage companyPage = new CompanyPage();
+    LoginPage loginPage = new LoginPage();
+
+
+    @Given("User logIn to the CRM")
+    public void userLogInToTheCRM() {
+        loginPage.login("marketing");
+    }
+
+
+
+    @When("User should go to the company page")
+    public void user_should_go_to_the_company_page() {
+        companyPage.CompanyPage.click();
+    }
+
+    @Then("User should see the following modules")
+    public void user_should_see_the_following_modules(List<String> expectedModules) {
+        List<String> actualModules = new ArrayList<>();
+        for (WebElement eachModules : companyPage.allModules) {
+            actualModules.add(eachModules.getText());
+            eachModules.isDisplayed();
+        }
+
+        Assert.assertEquals(actualModules, expectedModules);
+    }
+
+
+
 }
--- src/test/resources/features/US14_CompanyPageAccess.feature
+++ src/test/resources/features/US14_CompanyPageAccess.feature
@@ -1,1 +1,16 @@
-Feature: As a user, I should be able to access to the Company page
+Feature: User I should be able to access to the Company page
+
+  @wip_mo
+  Scenario: Users should be able to access the company page.
+    Given User logIn to the CRM
+    When User should go to the company page
+    Then User should see the following modules
+
+      | Official Information |
+      | Our Life             |
+      | About Company        |
+      | Photo Gallery        |
+      | Video                |
+      | Career               |
+      | Business News (RSS)  |
+      | More                 |
